6c0158cbe9839d2c59f72db31b52c58d5a46f820
Ajay Pradhan <ajay.pradhan@metacube.com>
2022-09-23 15:13:08 +0530

    Dataset

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Text;
 using System.Data.SqlClient;
 using System.Data;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace adoDotNetBasics;
 public class Program
@@ -27,7 +31,25 @@ public class Program
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                Console.WriteLine("Connected to database successfully!");
+                // Create 2 DataTable instances.
+                DataTable studentTable = new DataTable("students");
+                studentTable.Columns.Add("name");
+                studentTable.Columns.Add("id");
+                studentTable.Rows.Add("mary", 1);
+                studentTable.Rows.Add("amir", 2);
+
+                DataTable departmentTable = new DataTable("department");
+                departmentTable.Columns.Add("id");
+                departmentTable.Columns.Add("physics");
+                departmentTable.Rows.Add(1, "chemistry");
+                departmentTable.Rows.Add(2, "maths");
+
+                // Create a DataSet and put both tables in it.
+                DataSet set = new DataSet("college");
+                set.Tables.Add(studentTable);
+                set.Tables.Add(departmentTable);
+                // Visualize DataSet.
+                Console.WriteLine(PrettyXml(set.GetXml()));
             }
         }
         catch (Exception e)
@@ -36,4 +58,25 @@ public class Program
         }
 
     }
+
+    static string PrettyXml(string xml)
+    {
+        var stringBuilder = new StringBuilder();
+
+        var element = XElement.Parse(xml);
+
+        var settings = new XmlWriterSettings();
+        settings.OmitXmlDeclaration = true;
+        settings.Indent = true;
+        settings.NewLineOnAttributes = true;
+
+        using (var xmlWriter = XmlWriter.Create(stringBuilder, settings))
+        {
+            element.Save(xmlWriter);
+        }
+
+        return stringBuilder.ToString();
+    }
+
+
 }
