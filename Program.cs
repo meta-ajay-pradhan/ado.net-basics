46388352c5c3564d49f47616fb9e75cb8751c677
Ajay Pradhan <ajay.pradhan@metacube.com>
2022-09-23 14:42:49 +0530

    connection

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace adoDotNetBasics;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            string serverName = configuration.GetConnectionString("SERVER_NAME");
+            string database = configuration.GetConnectionString("DATABASE");
+            string username = configuration.GetConnectionString("USERNAME");
+            string password = configuration.GetConnectionString("PASSWORD");
+
+            string connectionString = $@"Data Source=${serverName};
+                                    Initial Catalog=${database};
+                                    User ID=${username};
+                                    Password=${password}";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Connected to database successfully!");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
