1254bc4bf968e91a1f48539d40d63ff46ec1495f
Ajay Pradhan <ajay.pradhan@metacube.com>
2022-09-26 10:55:43 +0530

    disconnectedEnvironment

--- Program.cs
+++ Program.cs
@@ -20,19 +20,59 @@ public class Program
             string username = configuration.GetConnectionString("USERNAME");
             string password = configuration.GetConnectionString("PASSWORD");
 
-            string connectionString = $@"Data Source=${serverName};
-                                    Initial Catalog=${database};
-                                    User ID=${username};
-                                    Password=${password}";
+            string connectionString = $"Data Source={serverName};Initial Catalog={database};User ID={username};Password={password}";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                Console.WriteLine("Connected to database successfully!");
+                //Create DataSet
+                string query = "Select id,username from Users";
+                DataSet ds = new DataSet();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(ds);
+
+                DataTable table = ds.Tables[0];
+
+                //Insert
+                table.Rows.Add(2, "Nikhil");
+                table.Rows.Add(1, "Debjyoti");
+                table.Rows.Add(3, "Rakshak");
+                table.Rows.Add(4, "Ritik");
+                table.Rows.Add(5, "Rahul");
+                table.Rows.Add(6, "Alan");
+                table.Rows.Add(7, "Karan");
+
+                DataRow[]? rows = table.Select();
+
+                //Delete
+                foreach(DataRow row in rows) {
+                    if(row["id"].ToString() == "7") {
+                        row.Delete();
+                        break;
+                    }
+                }
+                
+                //Update
+                rows = table.Select();
+
+                foreach(DataRow row in rows) {
+                    if(row["id"].ToString() == "6") {
+                        row["username"] = "Rajeshwar";
+                        break;
+                    }
+                }
+
+                //Select
+                rows = table.Select();
+
+                foreach(DataRow row in rows) {
+                    Console.WriteLine("id: {0}, name: {1}", row["id"].ToString(), row["username"].ToString());
+                }
             }
         }
         catch (Exception e)
         {
             Console.WriteLine(e.Message);
+            throw e;
         }
 
     }
